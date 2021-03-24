01906adf9110b13f6082d08db9f42f179b63d179
Leandro Pereira <leandro.pereira@microsoft.com>
2021-03-23 21:28:40 -0700

    Extract package version from debian/changelog

--- build.sh
+++ build.sh
@@ -4,18 +4,17 @@ rootdir=$(pwd)
 rm -rf build
 mkdir build
 
+pkgversion=$(grep -o -P -m 1 '^az-hibernate-agent \(\K([a-z0-9\.-]+)' debian/changelog | xargs)
+echo "Package version: ${pkgversion}"
+
 pushd build
 
 git clone -l ${rootdir}
 pushd az-hibernate-agent
-
 git switch main
-pkgversion=$(grep '$PackageVersion$ ' az-hibernate-agent.c | cut -d'$' -f3 | xargs)
+rm -rf .git
 popd
 
-echo "Package version: ${pkgversion}"
-
-rm -rf az-hibernate-agent/.git
 mv az-hibernate-agent az-hibernate-agent_${pkgversion}
 pkgnodebver=$(echo $pkgversion | cut -d'-' -f1)
 tar czvf ${rootdir}/build/az-hibernate-agent_${pkgnodebver}.orig.tar.gz az-hibernate-agent_${pkgversion}
